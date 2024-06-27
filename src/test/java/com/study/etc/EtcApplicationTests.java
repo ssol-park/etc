1627fd3d8e8ad3aad47764fb375d34e993cbb7b5
ssol-park <ksttlov.naver@hanmail.net>
2024-06-27 14:15:47 +0900

    initial commit

---
+++ src/test/java/com/study/etc/EtcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.etc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EtcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
