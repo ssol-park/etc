5f3c515163fe0451be2d44c1b92d8e97337aa776
ssol-park <ksttlov.naver@hanmail.net>
2024-10-13 21:53:42 +0900

    feat: ReentrantLock 예시 코드 추가

---
+++ src/test/java/com/study/etc/concurrent/day2/ReentrantLockTest.java
@@ -0,0 +1,119 @@
+package com.study.etc.concurrent.day2;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.ReentrantLock;
+
+class ReentrantLockTest {
+
+    Logger logger = LoggerFactory.getLogger(ReentrantLockTest.class);
+
+    public class Counter {
+        private final ReentrantLock lock = new ReentrantLock();
+        private int count = 0;
+
+        public void increament() {
+            lock.lock();
+
+            try {
+                this.count++;
+            } finally {
+                lock.unlock();
+            }
+        }
+        
+        public void increamentTryLock() {
+            if (lock.tryLock()) {
+                try {
+                    logger.info("[increamentTryLock] thread :: {}", Thread.currentThread().getName());
+                    this.count++;
+                } finally {
+                    lock.unlock();
+                }
+            } else {
+                logger.info("[increamentTryLock] Lock 을 획득하지 못함");
+            }
+        }
+
+        public void increamentTryLockTimeout() {
+            try {
+                if (lock.tryLock(1, TimeUnit.SECONDS)) {
+                    try {
+                        Thread.sleep(1500);
+                        this.count++;
+                    } finally {
+                        lock.unlock();
+                    }
+                } else {
+                    logger.info("[increamentTryLockTimeout] 1초 이내에 Lock 을 획득하지 못함");
+                }
+            } catch (InterruptedException e) {
+                logger.error("[InterruptedException] {}", e.getMessage());
+            }
+        }
+
+        public int getCount() {
+            return this.count;
+        }
+    }
+
+    @Test
+    void testCounter() throws InterruptedException {
+        Counter counter = new Counter();
+
+        Thread t1 = new Thread(() -> {
+            for (int i = 0; i < 10000; ++i) {
+                counter.increament();
+            }
+        });
+
+        Thread t2 = new Thread(() -> {
+            for (int i = 0; i < 10000; ++i) {
+                counter.increament();
+            }
+        });
+
+        t1.start();
+        t2.start();
+
+        t1.join();
+        t2.join();
+
+        logger.info("[testCounter] count :: {}", counter.getCount());
+    }
+
+    @Test
+    void testCounterTryLock() throws InterruptedException {
+        Counter counter = new Counter();
+
+        Thread t1 = new Thread(counter::increamentTryLock);
+        Thread t2 = new Thread(counter::increamentTryLock);
+
+        t1.start();
+        t2.start();
+
+        t1.join();
+        t2.join();
+
+        logger.info("[testCounterTryLock] count :: {}", counter.getCount());
+    }
+
+    @Test
+    void testCounterTryLockTimeout() throws InterruptedException {
+        Counter counter = new Counter();
+
+        Thread t1 = new Thread(counter::increamentTryLockTimeout);
+        Thread t2 = new Thread(counter::increamentTryLockTimeout);
+
+        t1.start();
+        t2.start();
+
+        t1.join();
+        t2.join();
+
+        logger.info("[testCounterTryLock] count :: {}", counter.getCount());
+    }
+}
