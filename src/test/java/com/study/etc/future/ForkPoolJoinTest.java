b5cba77c786912b9875ce2e8def520fdde33131e
ssol-park <ksttlov.naver@hanmail.net>
2024-10-21 22:08:24 +0900

    feat: ForkJoinPool 테스트 생성
     - parallelStream과 CompletableFuture의 기본 스레드 풀 동작 테스트
     - commonPool 사용 시 시간 지연에 따른 쓰레드 처리 테스트

---
+++ src/test/java/com/study/etc/future/ForkPoolJoinTest.java
@@ -0,0 +1,69 @@
+package com.study.etc.future;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+class ForkPoolJoinTest {
+
+    Logger logger = LoggerFactory.getLogger(ForkPoolJoinTest.class);
+
+    @Test
+    void commonPoolTest() {
+        logger.info("[commonPoolTest] ===================== ");
+
+        // CPU 의 코어 수 만큼 ForkJoinPool 이 생성됨
+        IntStream.rangeClosed(1, 1000).parallel()
+                        .mapToObj(n -> {
+                            try {
+                                logger.info("Sleep Start....");
+                                Thread.sleep(1000);
+                            } catch (InterruptedException e) {
+                                throw new RuntimeException(e);
+                            }
+                            return n * n;
+                        })
+                .forEach(r -> logger.info("{}", r));
+    }
+
+    @Test
+    void commonPoolTest2() {
+
+        List<CompletableFuture<Integer>> futures = IntStream.rangeClosed(1, 10).parallel()
+                .mapToObj(n -> {
+                    try {
+                        if (n % 6 == 0) {
+                            logger.info("Deep sleep ================ {}", n);
+                            Thread.sleep(10000);
+                        } else {
+                            logger.info("Running :: {}", n);
+                        }
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+
+                    return CompletableFuture.supplyAsync(() -> {
+                        int num = n * n;
+                        logger.info("[CompletableFuture] Return :: {}", num);
+                        return num;
+                    });
+                })
+                .collect(Collectors.toList());
+
+        CompletableFuture<Void> allOf = CompletableFuture.allOf(futures.toArray(new CompletableFuture[0]));
+
+        allOf.thenRun(() -> {
+            futures.forEach(future -> {
+                int num = future.join();
+                logger.info("future :: {}", num);
+            });
+        }).join();
+    }
+}
