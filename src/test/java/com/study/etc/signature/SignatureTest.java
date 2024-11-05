8a9337a356581c44fe579b07f21c9c8aee159972
ssol-park <ksttlov.naver@hanmail.net>
2024-11-05 21:58:15 +0900

    refactor: signature 테스트 패키지 생성

--- src/test/java/com/study/etc/signature/SignatureTest.java
+++ src/test/java/com/study/etc/signature/SignatureTest.java
@@ -1,6 +1,5 @@
-package com.study.etc;
+package com.study.etc.signature;
 
-import com.study.etc.signature.SignatureUtil;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
