3ab9a199f82cb85dc464455ed0249ac1dc1a6e6b
ssol-park <ksttlov.naver@hanmail.net>
2024-11-05 21:57:38 +0900

    refactor: 미사용 코드 제거

--- src/main/java/com/study/etc/dept/AppConfig.java
+++
@@ -1,9 +0,0 @@
-package com.study.etc.dept;
-
-import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@MapperScan("com.study.etc.dept")
-public class AppConfig {
-}
